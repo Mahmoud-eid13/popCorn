2283f1b11d76fe6715f532b5e30b1ac640a8404b
mahmoud.eid13 <eid.mahmoud1312@gmail.com>
2024-07-29 19:19:15 +0300

    updated

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React, { useState } from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+//import App from "./App";
+
+//import Test from "./Test";
+import Latest from "./Latest";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <Latest />
+  </React.StrictMode>
+);
